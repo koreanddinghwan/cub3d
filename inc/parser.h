54a5366cc473c12eb5bbc7bcd9cc3a2a2974b165
myukang <myunghwan0421@gmail.com>
2022-10-03 01:46:30 +0900

    FEAT:PARSER.h

--- inc/parser.h
+++ inc/parser.h
@@ -1,20 +1,28 @@
+#ifndef PARSER_H
+# define PARSER_H
+
 #include <fcntl.h>
 #include <errno.h>
 #include <stdio.h>
+#include "global.h"
 #include "../libft/libft.h"
 #include "../libft/get_next_line.h"
-typedef struct s_map
-{
-	void *NO;
-	void *SO;
-	void *WE;
-	void *EA;
-	int F[3];
-	int C[3];
-	int **map;
-} t_map;
 
+typedef enum idnt
+{
+	EMPTY = 0,
+	NO,
+	SO,
+	WE,
+	EA,
+	F,
+	C,
+	MAP,
+	END
+} e_symbol_id;
 
 void extension_checker(char *path, char *ext);
 int check_open(char *path);
 void fill_map(t_map *map, int fd);
+
+#endif
